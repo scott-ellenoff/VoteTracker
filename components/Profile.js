4f555083bb4dd0ef51df75cc3ea015dbaac3183b
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2018-11-12 19:25:40 -0600

    added main 3 components

---
+++ components/Profile.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React, {Component} from 'react';
+import {Platform, Text, View, StyleSheet} from 'react-native';
+
+export default class Profile extends Component {
+
+    render() {
+        return(
+            <Text style={styles.container}> Profile Page </Text>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 100,
+    paddingRight: 50,
+    paddingLeft: 50
+  },
+});
