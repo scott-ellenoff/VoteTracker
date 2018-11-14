e89b63bcb00c005e5c965405807aa01cdeaa0b7d
Zach Kamran <kamranzach@uchicago.edu>
2018-11-13 19:12:32 -0600

    got page working got new page to select senatiors

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 'use strict'
 
-
+import React, {Component} from 'react';
 
 import Home from './components/Home.js'
 import Main from './components/Main.js'
 import Profile from './components/Profile/Profile.js'
-
+import SelectLegislators from './components/Profile/selectLegislators';
 import {
-    createStackNavigator,
+  createStackNavigator,
 } from 'react-navigation'
 
 const App = createStackNavigator(
-    {
-        Home: {screen: Home},
-        Main: {screen: Main},
-        Profile: {screen: Profile}
-    }
+  {
+    Home: {screen: Home},
+    Main: {screen: Main},
+    Profile: {screen: Profile},
+    SelectLegislators: {screen: SelectLegislators}
+  }
 );
 
 export default App;
