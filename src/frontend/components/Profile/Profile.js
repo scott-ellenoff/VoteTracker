0c9a3b93fe923abc9f216c9e9880f366d3fea2e0
Zach Kamran <kamranzach@uchicago.edu>
2018-12-03 14:44:02 -0600

    fixed unfollow senator in profiule page

--- src/frontend/components/Profile/Profile.js
+++ src/frontend/components/Profile/Profile.js
@@ -2,13 +2,14 @@
 
 import React, {Component} from 'react';
 import axios from 'axios';
-import {FlatList, StyleSheet, View, Image, Text, TouchableHighlight, ScrollView} from 'react-native';
+import {FlatList, StyleSheet, View, Image, Text, TouchableHighlight, ScrollView, ListView} from 'react-native';
 import ListItem from './Row';
 import listData from './data';
 import SelectLegislators from './selectLegislators';
 import {AsyncStorage} from 'react-native'
 import Boot from "../Main/Boot";
 import {groupBy} from "./utils";
+import Swipeout from 'react-native-swipeout';
 
 // Alter defaults after instance has been created
 
@@ -110,7 +111,7 @@ export default class Profile extends Component {
   }
 
   updateFollowLegislators = () => {
-    const {legislators, axiosInstance, username, userID, token, followedLegislators} = this.state;
+    const {username, userID, token, followedLegislators} = this.state;
     const form = new FormData();
 
     form.append('username', username);
@@ -122,15 +123,15 @@ export default class Profile extends Component {
         "Authorization": `Token ${token}`
       }
     }).then(d => {
-      console.log(d.data);
       this.setState({followedLegislators: d.data.followed, matches: d.data.matched}, this.updateMatchData);
     })
       .catch((err) => console.log(err));
   };
 
   updateMatchData = () => {
+    console.log('here')
     const {token} = this.state;
-    this.setState({matchData: []});
+    // this.setState({matchData: []});
     const d = this.state.matches.reduce((acc, cur, idx) => {
         console.log(cur);
         const l = axios.get(cur, {
@@ -154,7 +155,7 @@ export default class Profile extends Component {
           }).catch(err => console.log(err));
       }, []
     );
-  }
+  };
 
   renderSeparator() {
     return (
@@ -179,7 +180,6 @@ export default class Profile extends Component {
   }
 
   renderItem(item) {
-    console.log(this.state.matchData);
     const name = this.state.byLid[item.split('/')[6]][0].fullname;
       const val = this.state.matchData.findIndex((el) => el.legislator === item);
       const matchPctPromise = ` %`;
@@ -234,6 +234,63 @@ export default class Profile extends Component {
     }
   };
 
+  deleteLegislator = (rowData) => {
+    this.setState((prevState) => {
+      const updated = prevState.userInfo;
+      console.log(rowData);
+      const filtered = prevState.followedLegislators.filter(function (item) {
+        return rowData !== item
+      });
+
+      updated.followed = [...filtered];
+      console.log(filtered);
+      AsyncStorage.setItem('user', JSON.stringify([updated]));
+      console.log(prevState.followedLegislators, 'in updatelegislators');
+      return {
+        followedLegislators: [...filtered],
+        pickerSelected: filtered[0].split('/')[6],
+        addMode: false
+      }
+    }, this.updateFollowLegislators)
+  } ;
+
+  viewLegislator = (legislator) => {
+
+  };
+
+  renderRow(item) {
+    console.log('here');
+    let swipeBtns = [{
+      text: 'Unfollow',
+      backgroundColor: 'red',
+      onPress: () => { this.deleteLegislator(item) }
+    }];
+    const name = this.state.byLid[item.split('/')[6]][0].fullname;
+    const val = this.state.matchData.findIndex((el) => el.legislator === item);
+    const matchPctPromise = ` %`;
+    let pct;
+    if (val === -1){
+      pct = ''
+    }else {
+      pct = this.state.matchData[0].pct;
+    }
+    return (
+      <Swipeout right={swipeBtns}
+                autoClose={true}
+                backgroundColor= 'transparent'>
+        <TouchableHighlight
+          // underlayColor='rgba(192,192,192,1,0.6)'
+          onPress={this.viewLegislator.bind(this, item)} >
+          <View>
+            <View >
+              <Text> {`Legislator: ${name} Match Pct: ${pct}`} </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeout>
+    )
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     let {addMode, loading, legislators, pickerSelected, followedLegislators} = this.state;
@@ -253,13 +310,14 @@ export default class Profile extends Component {
             </TouchableHighlight>
           </View>
           <View>
-            <FlatList
-              style={this.props.style}
-              data={this.state.followedLegislators}
-              ItemSeparatorComponent={this.renderSeparator}
-              renderItem={({item}) => this.renderItem(item)}
-              scrollEnabled={this.state.enable}
-            />
+              <FlatList
+                style={this.props.style}
+                data={this.state.followedLegislators}
+                ItemSeparatorComponent={this.renderSeparator}
+                renderItem={({item}) => this.renderRow(item)}
+                scrollEnabled={this.state.enable}
+              />
+
           </View>
 
         </View>
@@ -279,7 +337,7 @@ export default class Profile extends Component {
               style={this.props.style}
               data={this.state.followedLegislators}
               ItemSeparatorComponent={this.renderSeparator}
-              renderItem={({item}) => this.renderItem(item)}
+              renderItem={({item}) => this.renderRow(item)}
               scrollEnabled={this.state.enable}
             />
           </View>
